e5c56b909fda16758ed1fd3f38df7f5c73743dd9
Akrom <akromxolboyev0615@gmail.com>
2024-08-28 18:04:43 +0500

    .

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>Page</div>
+    <div
+     className='w-[1200px] mx-auto'>
+      <h1 className="flex justify-center gap-5 p-5 bg-slate-300 mb-5">ABOUT</h1>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas expedita nam amet, laborum veritatis voluptatum soluta deleniti quam sapiente dolores repellat ut eos corporis ratione iusto officia illo repellendus nobis aspernatur! Doloribus ratione nobis numquam. Est placeat dolores illo voluptatibus, officiis eius distinctio fuga nam repellendus molestias. In quibusdam quis perspiciatis voluptates placeat explicabo hic, quae exercitationem aut nemo, similique maiores labore rerum. Nesciunt provident ullam vel officia rerum obcaecati voluptate ad labore. Nisi quas, recusandae explicabo velit sint perspiciatis asperiores aperiam esse repellendus similique molestias earum ipsa, modi vero obcaecati fugit eum blanditiis id non sit consequatur quaerat omnis.</div>
+
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
-
+import Link from "next/link";
 export default function Home() {
   return (
     <>
-    <h1>salom</h1>
+   <div>
+  <div className="flex justify-center gap-10 p-5 bg-slate-300 mb-5">
+  <Link href="/about">ABOUT</Link>
+  <Link href="/users">USERS</Link>
+  </div>
+    <div className="w-[1200px] mx-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti soluta id reprehenderit pariatur dolor accusantium vitae illo! Optio blanditiis est velit commodi ut quo qui molestiae illo! Ipsam neque doloremque sit eos error suscipit, accusantium maxime voluptate, in veritatis esse beatae adipisci vel commodi distinctio quis. Corrupti quia pariatur rem, explicabo illo officiis eum molestiae aliquid aut vero, reprehenderit, delectus adipisci? Dolorum velit officiis blanditiis eius temporibus animi tempora debitis repudiandae porro reiciendis impedit non ex, quia fugiat nihil. Possimus, non. Soluta, fugiat dolor nam nihil sunt ea consequuntur doloremque, expedita voluptatem est repellat molestiae odit. Nostrum consequuntur, architecto mollitia eveniet temporibus repellendus dolorum debitis cupiditate delectus optio saepe dolorem laboriosam eligendi suscipit sequi ipsa fugit accusantium voluptate laudantium minima. Aut fuga dolorem, commodi corporis sapiente fugit ipsa provident, itaque quo quas beatae nulla? Consectetur expedita commodi deleniti porro temporibus iusto beatae! Sit, assumenda, ea consequatur nisi vel commodi doloribus dolor tenetur iste ratione facere. Quaerat suscipit deleniti, tenetur maiores, et, ratione explicabo quas voluptate odit expedita ipsam error exercitationem quasi at harum aliquam totam aliquid praesentium dicta architecto. Optio omnis ullam, doloribus dolorum voluptas dolores illum! Accusantium totam voluptatibus, eos quod ipsa quae, placeat officiis, reprehenderit aperiam vero neque.</div>
+   </div>
     </>
   );
 }
